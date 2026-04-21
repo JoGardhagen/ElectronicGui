295ade6345c6de3ce7b1497d325cebb9c69a574c
Joakim <superapa.spanx@gmail.com>
2026-04-21 08:06:47 +0200

    AddingStuff

--- src/Circuits/Circuit6.cpp
+++ src/Circuits/Circuit6.cpp
@@ -30,37 +30,38 @@ void DrawCircuit6_Basic(AppState& state, IGPIO* gpio){
     };
     
     // Rita en 8-pin IC (som 555)
-    auto ic555 = DrawIC(350, 250, 8, "555", "IC-555", pinDescs555);
+    auto ic555 = DrawIC(350, 250, 8, "555", "NE555P_Ic 555-Timer", pinDescs555);
 
     Vector2 IcGndPin = ic555.pins[0];
-    DrawGPIOPin(IcGndPin.x-200,IcGndPin.y,"GND","GPIO_GND");
-    DrawWire(IcGndPin.x,IcGndPin.y,IcGndPin.x-200,IcGndPin.y);
+    //==========GND_WIRE
+    DrawGPIOPin(100,IcGndPin.y,"GND","GPIO_GND");
+    DrawWire(100,IcGndPin.y,IcGndPin.x,IcGndPin.y);
+
 
     Vector2 VccPin = ic555.pins[7];
-    DrawWire(VccPin.x,VccPin.y,VccPin.x+100,VccPin.y);
-    DrawWire(VccPin.x+100,VccPin.y,VccPin.x+100,VccPin.y-70);
-    DrawGPIOPin(VccPin.x+100,VccPin.y-70,"+5Vcc","GPIO +5V");
+    //=============VccWire
+    DrawGPIOPin(100,VccPin.y-70,"+5Vcc","GPIO +5V");
+    DrawWire(100,VccPin.y-70,100,VccPin.y-30);
+    DrawWire(100,VccPin.y-30,410,VccPin.y-30);
+    DrawWire(410,VccPin.y-30,410,VccPin.y);
+    DrawWire(410,VccPin.y,VccPin.x,VccPin.y);
+    //====================
 
-    DrawWire(VccPin.x+100,VccPin.y-70,VccPin.x,VccPin.y-70);
+
     DrawResistor(VccPin.x-60,VccPin.y-70,"R1","10k Ohm resistor");
-    DrawWire(VccPin.x-60,VccPin.y-70,VccPin.x-80,VccPin.y-70);
+    
     DrawResistor(VccPin.x-140,VccPin.y-70,"R2","100k Ohm resistor");
-    DrawWire(VccPin.x-70,VccPin.y-70,VccPin.x-70,VccPin.y-40);
-    DrawWire(VccPin.x-70,VccPin.y-40,VccPin.x-220,VccPin.y-40);
-    DrawWire(VccPin.x-220,VccPin.y-40,VccPin.x-220,VccPin.y+30);
-    DrawWire(VccPin.x-220,VccPin.y+30,VccPin.x-220,VccPin.y+50);
+    
 
-    DrawCapacitor(VccPin.x-220,VccPin.y+50,"C1","10uF Kondensator");
+    //DrawCapacitor(VccPin.x-200,VccPin.y+50,"C1","10uF Kondensator");
+
 
     Vector2 OutPin = ic555.pins[2];
-    DrawWire(OutPin.x,OutPin.y,OutPin.x-30,OutPin.y);
-    DrawWire(OutPin.x-30,OutPin.y,OutPin.x-30,OutPin.y+100);
-    DrawWire(OutPin.x-30,OutPin.y+100,OutPin.x-50,OutPin.y+100);
+
     DrawResistor(OutPin.x-110,OutPin.y+100,"R220","220 Ohm Resistor");
-    DrawWire(OutPin.x-110,OutPin.y+100,OutPin.x-120,OutPin.y+100);
+
     DrawLED(OutPin.x-160,OutPin.y+100,false,"Lysdiod");
-    DrawWire(OutPin.x-160,OutPin.y+100,OutPin.x-200,OutPin.y+100);
-    DrawWire(OutPin.x-200,OutPin.y+100,IcGndPin.x-200,IcGndPin.y);
+
 
 
 
